4e475f88de31a29f7b8ebda3b908a9e313f029b9
Semicolon <gamingmasterahmad@gmail.com>
2023-09-01 23:12:19 +0300

    Bulk messaging phase 01

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+
+$_SESSION["api_instance_id"] = "7103852504";
+$_SESSION["api_token"] = "f49e95943155403688e86b18ec641b9dcfe4db5e16b44fc4ba";
+
 if(($_SESSION["userid"]) > 0) {
     header('Location: public/dashboard.php');
 }else
