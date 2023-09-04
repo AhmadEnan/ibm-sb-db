eb09f65d43cd0af16b212cd1437b3eba80bff50f
AhmedKhairi2 <112388142+AhmedKhairi2@users.noreply.github.com>
2023-09-04 21:38:29 +0300

    Login page done

--- public/login.php
+++ public/login.php
@@ -1,23 +1,53 @@
-<!-- login.php -->
-<html>
-    <head>
-    <title>IBM SB - Login</title>
-    <link rel="stylesheet" href="styles/index.css">
+<!doctype html>
+<html lang="en">
+<head>
+  <title>Webleb</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
-    <h1>Login</h1>
-    <form id="loginForm" action="../server/controllers/authenticate.php" method="POST">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="example@gmail.com" required><br>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" placeholder="Password" required><br>
-
-        <button type="submit" name="submit">Login</button>
-    </form>
-
-    <?php 
-    
-    ?>
+<div id="stars"></div>
+<div id="stars2"></div>
+<div id="stars3"></div>
+<div class="section">
+  <div class="container">
+    <div class="row full-height justify-content-center">
+      <div class="col-12 text-center align-self-center py-5">
+        <div class="section pb-5 pt-5 pt-sm-2 text-center">
+        <img src="https://www.freepnglogos.com/uploads/ibm-logo-png/ibm-logo-png-transparent-svg-vector-bie-supply-3.png">
+          <div class="card-3d-wrap mx-auto">
+            <div class="card-3d-wrapper">
+              <div class="card-front">
+                <div class="center-wrap">
+                  <div class="section text-center">
+                    <h4 class="mb-4 pb-3">Log In</h4>
+                    <div class="form-group">
+                      <input type="email" class="form-style" placeholder="Email">
+                      <i class="input-icon uil uil-at"></i>
+                    </div>	
+                    <div class="form-group mt-2">
+                      <input type="password" class="form-style" placeholder="Password">
+                      <i class="input-icon uil uil-lock-alt"></i>
+                    </div>
+                    <a href="" class="btn mt-4">Login</a>
+                    <p class="mb-0 mt-4 text-center"><a href="" class="link">Forgot your password?</a></p>
+                      </div>
+                    </div>
+                  </div>
+              <div class="card-back">
+                <div class="center-wrap">
+                  <div class="section text-center">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
