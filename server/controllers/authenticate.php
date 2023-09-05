14d50b358fc92d14eeea9260d7665543eba98fc7
Semicolon <gamingmasterahmad@gmail.com>
2023-09-05 03:23:57 +0300

    fix #2

--- server/controllers/authenticate.php
+++ server/controllers/authenticate.php
@@ -9,13 +9,7 @@ if(!(isset($_POST["submit"])))
 // Get user inputs from the form
 $email = $_POST['email'];
 $password = $_POST['password'];
-$forgot = $_POST['forgot'];
 require_once "dbh.php";
 require_once "../functions/functions.php";
-
-if($forgot !== 0)
-{
-
-}
 
 authUser($conn, $email, $password);
